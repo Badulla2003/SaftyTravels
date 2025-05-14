9b18c673c2d8ee2c3b0caaf64934bfb55fe0f13b
Badulla <siaramayodya@gmail.com>
2025-05-14 15:06:39 +0530

    sink

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const axiosRequest = axios.create({
 
     // baseURL: 'http://localhost:8080', kings building // Replace this with your API's base URL
-    baseURL:'https://saftytravelscode-production.up.railway.app/',
+    baseURL:'https://saftytravelscode-production.up.railway.app',
     headers: {
       'Authorization': 'Bearer your_token_here', // Add your default authorization header if needed
       'Content-Type': 'application/json', // Set the default content type for requests
